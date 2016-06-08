423928dff6684cb788c69960aca47c8faf2fd7b8
Alexander Mahrt <alexander.mahrt@aboutyou.de>
2016-06-08 13:10:18 +0200

    Implemented queries and entities.

---
+++ src/LeadCommerce/Shopware/SDK/Entity/Country.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * LeadCommerce\Shopware\SDK\Entity
+ *
+ * Copyright 2016 LeadCommerce
+ *
+ * @author Alexander Mahrt <amahrt@leadcommerce.de>
+ * @copyright 2016 LeadCommerce <amahrt@leadcommerce.de>
+ */
+
+namespace LeadCommerce\Shopware\SDK\Entity;
+
+
+class Country extends Base
+{
+
+}
---
+++ src/LeadCommerce/Shopware/SDK/Query/DispatchQuery.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * LeadCommerce\Shopware\SDK\Query
+ *
+ * Copyright 2016 LeadCommerce
+ *
+ * @author Alexander Mahrt <amahrt@leadcommerce.de>
+ * @copyright 2016 LeadCommerce <amahrt@leadcommerce.de>
+ */
+
+namespace LeadCommerce\Shopware\SDK\Query;
+
+
+class DispatchQuery extends Base
+{
+
+    /**
+     * @return mixed
+     */
+    protected function getClass()
+    {
+        return 'LeadCommerce\\Shopware\\SDK\\Entity\\Dispatch';
+    }
+}
