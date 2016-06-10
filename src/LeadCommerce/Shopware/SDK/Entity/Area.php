036d480a142710229d077b53c8a062b176c6d0f6
Alexander Mahrt <hello@youngandcreative.de>
2016-06-10 21:33:06 +0200

    * Area has now getters and setters.

--- src/LeadCommerce/Shopware/SDK/Entity/Area.php
+++ src/LeadCommerce/Shopware/SDK/Entity/Area.php
@@ -11,7 +11,99 @@
 namespace LeadCommerce\Shopware\SDK\Entity;
 
 
+/**
+ * Class Area
+ * @package LeadCommerce\Shopware\SDK\Entity
+ */
 class Area extends Base
 {
+    /**
+     * @var int
+     */
+    protected  $id;
+    /**
+     * @var string
+     */
+    protected  $name;
+    /**
+     * @var bool
+     */
+    protected  $active;
+    /**
+     * @var Country[]
+     */
+    protected  $countries;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Area
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Area
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isActive()
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param boolean $active
+     * @return Area
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+        return $this;
+    }
+
+    /**
+     * @return Country[]
+     */
+    public function getCountries()
+    {
+        return $this->countries;
+    }
+
+    /**
+     * @param Country[] $countries
+     * @return Area
+     */
+    public function setCountries($countries)
+    {
+        $this->countries = $countries;
+        return $this;
+    }
 
 }
