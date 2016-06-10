d801f3df1d0a00a23173df0da2bd7adc9eba16bb
Alexander Mahrt <hello@youngandcreative.de>
2016-06-10 21:31:46 +0200

    * Address has now getters and setters.

--- src/LeadCommerce/Shopware/SDK/Entity/Address.php
+++ src/LeadCommerce/Shopware/SDK/Entity/Address.php
@@ -11,7 +11,362 @@
 namespace LeadCommerce\Shopware\SDK\Entity;
 
 
+/**
+ * Class Address
+ * @package LeadCommerce\Shopware\SDK\Entity
+ */
 class Address extends Base
 {
+    /**
+     * @var int
+     */
+    protected  $id;
+    /**
+     * @var string
+     */
+    protected  $company;
+    /**
+     * @var string
+     */
+    protected  $department;
+    /**
+     * @var string
+     */
+    protected  $salutation;
+    /**
+     * @var string
+     */
+    protected  $firstname;
+    /**
+     * @var string
+     */
+    protected  $lastname;
+    /**
+     * @var string
+     */
+    protected  $street;
+    /**
+     * @var string
+     */
+    protected  $zipcode;
+    /**
+     * @var string
+     */
+    protected  $city;
+    /**
+     * @var string
+     */
+    protected  $phone;
+    /**
+     * @var int
+     */
+    protected  $vatId;
+    /**
+     * @var string
+     */
+    protected  $additionalAddressLine1;
+    /**
+     * @var string
+     */
+    protected  $additionalAddressLine2;
+    /**
+     * @var int
+     */
+    protected  $country;
+    /**
+     * @var int
+     */
+    protected  $state;
+    /**
+     * @var array
+     */
+    protected  $attribute;
 
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Address
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * @param string $company
+     * @return Address
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDepartment()
+    {
+        return $this->department;
+    }
+
+    /**
+     * @param string $department
+     * @return Address
+     */
+    public function setDepartment($department)
+    {
+        $this->department = $department;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSalutation()
+    {
+        return $this->salutation;
+    }
+
+    /**
+     * @param string $salutation
+     * @return Address
+     */
+    public function setSalutation($salutation)
+    {
+        $this->salutation = $salutation;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $firstname
+     * @return Address
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     * @return Address
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * @param string $street
+     * @return Address
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getZipcode()
+    {
+        return $this->zipcode;
+    }
+
+    /**
+     * @param string $zipcode
+     * @return Address
+     */
+    public function setZipcode($zipcode)
+    {
+        $this->zipcode = $zipcode;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     * @return Address
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     * @return Address
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVatId()
+    {
+        return $this->vatId;
+    }
+
+    /**
+     * @param int $vatId
+     * @return Address
+     */
+    public function setVatId($vatId)
+    {
+        $this->vatId = $vatId;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdditionalAddressLine1()
+    {
+        return $this->additionalAddressLine1;
+    }
+
+    /**
+     * @param string $additionalAddressLine1
+     * @return Address
+     */
+    public function setAdditionalAddressLine1($additionalAddressLine1)
+    {
+        $this->additionalAddressLine1 = $additionalAddressLine1;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdditionalAddressLine2()
+    {
+        return $this->additionalAddressLine2;
+    }
+
+    /**
+     * @param string $additionalAddressLine2
+     * @return Address
+     */
+    public function setAdditionalAddressLine2($additionalAddressLine2)
+    {
+        $this->additionalAddressLine2 = $additionalAddressLine2;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param int $country
+     * @return Address
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param int $state
+     * @return Address
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAttribute()
+    {
+        return $this->attribute;
+    }
+
+    /**
+     * @param array $attribute
+     * @return Address
+     */
+    public function setAttribute($attribute)
+    {
+        $this->attribute = $attribute;
+        return $this;
+    }
 }
