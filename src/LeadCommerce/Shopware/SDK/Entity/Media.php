e453d03b71b0b7d1150cf2a8eb56504da3044f1d
Leopold Engst <engst@portrino.de>
2017-11-10 09:46:22 +0100

    * Add Media Entity

---
+++ src/LeadCommerce/Shopware/SDK/Entity/Media.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * LeadCommerce\Shopware\SDK\Entity
+ *
+ * Copyright 2016 LeadCommerce
+ *
+ * @author Alexander Mahrt <amahrt@leadcommerce.de>
+ * @copyright 2016 LeadCommerce <amahrt@leadcommerce.de>
+ */
+namespace LeadCommerce\Shopware\SDK\Entity;
+
+/**
+ * Class Media
+ * @package LeadCommerce\Shopware\SDK\Entity
+ */
+class Media extends Base
+{
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var int
+     */
+    protected $album;
+    /**
+     * @var string
+     */
+    protected $file;
+    /**
+     * @var string
+     */
+    protected $description;
+    /**
+     * @var string
+     */
+    protected $path;
+
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return Media
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAlbum()
+    {
+        return $this->album;
+    }
+
+    /**
+     * @param int $album
+     *
+     * @return Media
+     */
+    public function setAlbum($album)
+    {
+        $this->album = $album;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     *
+     * @return Media
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * @param string $file
+     *
+     * @return Media
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return Media
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+}
