9aad22dc0f6d0c4e59c3f49643bcecf1ae1f49ab
Leopold Engst <engst@portrino.de>
2018-01-15 16:15:51 +0100

    [TASK] add Order entity refs #36883

---
+++ src/LeadCommerce/Shopware/SDK/Entity/Order.php
@@ -0,0 +1,299 @@
+<?php
+namespace Portrino\LeadCommerce\Shopware\SDK\Entity;
+
+/**
+ * Class Order
+ *
+ * @package Portrino\LeadCommerce\Shopware\SDK\Entity
+ */
+class Order extends \LeadCommerce\Shopware\SDK\Entity\Base
+{
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var string
+     */
+    protected $number;
+    /**
+     * @var mixed
+     */
+    protected $customerId;
+    /**
+     * @var string
+     */
+    protected $invoiceAmount;
+    /**
+     * @var string
+     */
+    protected $invoiceAmountNet;
+    /**
+     * @var string
+     */
+    protected $invoiceShipping;
+    /**
+     * @var string
+     */
+    protected $invoiceShippingNet;
+    /**
+     * @var string
+     */
+    protected $currency;
+    /**
+     * @var string
+     */
+    protected $remoteAddress;
+    /**
+     * @var string
+     */
+    protected $orderTime;
+    /**
+     * @var string
+     */
+    protected $transactionId;
+
+    /**
+     * @var OrderBilling[]
+     */
+    protected $billing;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return Order
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTransactionId()
+    {
+        return $this->transactionId;
+    }
+
+    /**
+     * @param $transactionId
+     * @return Order
+     */
+    public function setTransactionId($transactionId)
+    {
+        $this->transactionId = $transactionId;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * @param string $number
+     *
+     * @return Order
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCustomerId()
+    {
+        return $this->customerId;
+    }
+
+    /**
+     * @param string $customerId
+     *
+     * @return Order
+     */
+    public function setCustomerId($customerId)
+    {
+        $this->customerId = $customerId;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInvoiceAmount()
+    {
+        return $this->invoiceAmount;
+    }
+
+    /**
+     * @param string $invoiceAmount
+     *
+     * @return Order
+     */
+    public function setInvoiceAmount($invoiceAmount)
+    {
+        $this->invoiceAmount = $invoiceAmount;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInvoiceAmountNet()
+    {
+        return $this->invoiceAmountNet;
+    }
+
+    /**
+     * @param string $invoiceAmountNet
+     *
+     * @return Order
+     */
+    public function setInvoiceAmountNet($invoiceAmountNet)
+    {
+        $this->invoiceAmountNet = $invoiceAmountNet;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInvoiceShipping()
+    {
+        return $this->invoiceShipping;
+    }
+
+    /**
+     * @param string $invoiceShipping
+     *
+     * @return Order
+     */
+    public function setInvoiceShipping($invoiceShipping)
+    {
+        $this->invoiceShipping = $invoiceShipping;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInvoiceShippingNet()
+    {
+        return $this->invoiceShippingNet;
+    }
+
+    /**
+     * @param string $invoiceShippingNet
+     *
+     * @return Order
+     */
+    public function setInvoiceShippingNet($invoiceShippingNet)
+    {
+        $this->invoiceShippingNet = $invoiceShippingNet;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param string $currency
+     *
+     * @return Order
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRemoteAddress()
+    {
+        return $this->remoteAddress;
+    }
+
+    /**
+     * @param string $remoteAddress
+     *
+     * @return Order
+     */
+    public function setRemoteAddress($remoteAddress)
+    {
+        $this->remoteAddress = $remoteAddress;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrderTime()
+    {
+        return $this->orderTime;
+    }
+
+    /**
+     * @param string $orderTime
+     *
+     * @return Order
+     */
+    public function setOrderTime($orderTime)
+    {
+        $this->orderTime = $orderTime;
+
+        return $this;
+    }
+
+    /**
+     * @return OrderBilling[]
+     */
+    public function getBilling()
+    {
+        return $this->billing;
+    }
+
+    /**
+     * @param OrderBilling[] $billing
+     *
+     * @return Order
+     */
+    public function setBilling($billing)
+    {
+        $this->billing = $billing;
+
+        return $this;
+    }
+}
---
+++ src/LeadCommerce/Shopware/SDK/Entity/OrderBilling.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * LeadCommerce\Shopware\SDK\Entity
+ *
+ * Copyright 2016 LeadCommerce
+ *
+ * @author Alexander Mahrt <amahrt@leadcommerce.de>
+ * @copyright 2016 LeadCommerce <amahrt@leadcommerce.de>
+ */
+namespace LeadCommerce\Shopware\SDK\Entity;
+
+/**
+ * Class OrderBilling
+ *
+ * @package LeadCommerce\Shopware\SDK\Entity
+ */
+class OrderBilling extends Base
+{
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var string
+     */
+    protected $firstname;
+    /**
+     * @var string
+     */
+    protected $lastname;
+    /**
+     * @var string
+     */
+    protected $street;
+    /**
+     * @var string
+     */
+    protected $zipcode;
+    /**
+     * @var string
+     */
+    protected $city;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return OrderBilling
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $firstname
+     *
+     * @return OrderBilling
+     */
+    public function setFirstName($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     *
+     * @return OrderBilling
+     */
+    public function setLastName($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * @param $street
+     * @return OrderBilling
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getZipcode()
+    {
+        return $this->zipcode;
+    }
+
+    /**
+     * @param $zipcode
+     * @return OrderBilling
+     */
+    public function setZipcode($zipcode)
+    {
+        $this->zipcode = $zipcode;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param $city
+     * @return OrderBilling
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+}
